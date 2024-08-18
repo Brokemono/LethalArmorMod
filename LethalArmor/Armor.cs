da4722b842f0eb6da203ba735735f1e33c5cb10e
David <davidkostic2309@gmail.com>
2024-08-18 14:56:46 +0200

    Add project files.

---
+++ LethalArmor/Armor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LethalArmor
+{
+    public class Armor
+    {
+        public int Durability { get; private set; }
+        public bool IsEquipped { get; private set; }
+
+        public Armor(int initialDurability)
+        {
+            Durability = initialDurability;
+            IsEquipped = true; // Assuming armor is equipped when created
+        }
+
+        public void TakeDamage()
+        {
+            if (Durability > 0)
+            {
+                Durability--;
+            }
+            if (Durability <= 0)
+            {
+                BreakArmor();
+            }
+        }
+
+        private void BreakArmor()
+        {
+            IsEquipped = false;
+            // Additional logic for when the armor breaks
+        }
+    }
+}
---
+++ LethalArmor/Plugin.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using LethalArmor.PlayerControllerBPatch;
+
+namespace LethalArmor
+{
+    [BepInPlugin("Brokemono.LethalArmor", "Lethal Armor", "1.0.0")]
+    public class ArmorBase : BaseUnityPlugin
+    {
+        private static ArmorBase Instance;
+        internal ManualLogSource mLs;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            mLs = BepInEx.Logging.Logger.CreateLogSource("LethalArmor");
+
+            mLs.LogInfo("Armor is born!");
+
+            var harmony = new Harmony("Brokemono.LethalArmor");
+            harmony.PatchAll(typeof(PlayerControllerBPatch.DamagePlayer_Patch));
+        }
+    }
+}
